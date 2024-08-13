59d588183df415e6a3e7e146231ff6bde1b71575
Diego Ospina <140738227+diegospina1@users.noreply.github.com>
2024-08-13 09:01:06 -0500

    Add files via upload
    Detallando paso a paso de las listas.

--- src/main/java/co/edu/ue/Main.java
+++ src/main/java/co/edu/ue/Main.java
@@ -1,27 +1,155 @@
 package co.edu.ue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import co.edu.ue.services.Ordenamiento;
+
+//Presentado por: Danna García y Diego Ospina.
+//Programa: Ingeniería de software. 14A
+//Estructura de datos, Algoritmos de ordenamiento
 
 public class Main {
 
     public static void main(String[] args) {
-
         Ordenamiento ordenamiento = new Ordenamiento();
 
-        ordenamiento.addFirstElements();
-        ordenamiento.addSecondElements();
-        ordenamiento.addThirdElements();
-        ordenamiento.addFourthElements();
+        Integer[] first = new Integer[]{40, 21, 4, 9, 10, 35};
+        List<Integer> firstList =  Arrays.asList(first);
+        Integer[] second = new Integer[]{47, 1, 5, 3, 7, 13, 25, 60, 0};
+        List<Integer> secondList = Arrays.asList(second);
+        Integer[] third = new Integer[]{5, 2, 7, 3, 1, 8, 2, 6, 9};
+        List<Integer> thirdList = Arrays.asList(third);
+        Integer[] fourth = new Integer[]{3, 5, 33, 1, 8, 1, 24, 23, 8};
+        List<Integer> fourthList = Arrays.asList(fourth);
 
-        List<Integer> firstList = ordenamiento.firstList;
-        List<Integer> secondList = ordenamiento.secondList;
-        List<Integer> thirdList = ordenamiento.thirdList;
-        List<Integer> fourthList = ordenamiento.fourthList;
+        //Punto 1 (Array 1: {40, 21, 4, 9, 10, 35})
+        System.out.println("\nPRIMERA LISTA");
+        System.out.println("SELECCIÓN ----\n");
 
-        ordenamiento.algoritmoSeleccion(firstList);
+//        ordenamiento.algoritmoSeleccion(firstList, false);
+        System.out.println();
+        ordenamiento.algoritmoSeleccion(firstList, true);
 
+//        System.out.println("\nBURBUJA ----\n");
+//
+//        ordenamiento.algoritmoBurbuja(firstList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoBurbuja(firstList, true);
+//
+//        System.out.println("\nINSERCIÓN DIRECTA ----\n");
+//
+//        ordenamiento.algoritmoInsercionDirecta(firstList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoInsercionDirecta(firstList, true);
+//
+//        System.out.println("\nSHELL ----\n");
+//
+//        ordenamiento.algoritmoShell(firstList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoShell(firstList, true);
+//
+//        System.out.println("\nQUICKSORT ----\n");
+//
+//        ordenamiento.algoritmoQuickSort(firstList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoQuickSort(firstList, true);
+//
+//        //Punto 2 (Array 2: {47, 1, 5, 3, 7, 13, 25, 60, 0})
+//        System.out.println("\nSEGUNDA LISTA");
+//        System.out.println("SELECCIÓN ----\n");
+//
+//        ordenamiento.algoritmoSeleccion(secondList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoSeleccion(secondList, true);
+//
+//        System.out.println("\nBURBUJA ----\n");
+//
+//        ordenamiento.algoritmoBurbuja(secondList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoBurbuja(secondList, true);
+//
+//        System.out.println("\nINSERCIÓN DIRECTA ----\n");
+//
+//        ordenamiento.algoritmoInsercionDirecta(secondList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoInsercionDirecta(secondList, true);
+//
+//        System.out.println("\nSHELL ----\n");
+//
+//        ordenamiento.algoritmoShell(secondList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoShell(secondList, true);
+//
+//        System.out.println("\nQUICKSORT ----\n");
+//
+//        ordenamiento.algoritmoQuickSort(secondList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoQuickSort(secondList, true);
+//
+//        //Punto 3 (Array 3: {5, 2, 7, 3, 1, 8, 2, 6, 9})
+//        System.out.println("\nTERCERA LISTA");
+//        System.out.println("SELECCIÓN ----\n");
+//
+//        ordenamiento.algoritmoSeleccion(thirdList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoSeleccion(thirdList, true);
+//
+//        System.out.println("\nBURBUJA ----\n");
+//
+//        ordenamiento.algoritmoBurbuja(thirdList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoBurbuja(thirdList, true);
+//
+//        System.out.println("\nINSERCIÓN DIRECTA ----\n");
+//
+//        ordenamiento.algoritmoInsercionDirecta(thirdList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoInsercionDirecta(thirdList, true);
+//
+//        System.out.println("\nSHELL ----\n");
+//
+//        ordenamiento.algoritmoShell(thirdList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoShell(thirdList, true);
+//
+//        System.out.println("\nQUICKSORT ----\n");
+//
+//        ordenamiento.algoritmoQuickSort(thirdList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoQuickSort(thirdList, true);
+//
+//        //Punto 4 (Array 4: {3, 5, 33, 1, 8, 1, 24, 23, 8})
+//        System.out.println("\nCUARTA LISTA");
+//        System.out.println("SELECCIÓN ----\n");
+//
+//        ordenamiento.algoritmoSeleccion(fourthList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoSeleccion(fourthList, true);
+//
+//        System.out.println("\nBURBUJA ----\n");
+//
+//        ordenamiento.algoritmoBurbuja(fourthList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoBurbuja(fourthList, true);
+//
+//        System.out.println("\nINSERCIÓN DIRECTA ----\n");
+//
+//        ordenamiento.algoritmoInsercionDirecta(fourthList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoInsercionDirecta(fourthList, true);
+//
+//        System.out.println("\nSHELL ----\n");
+//
+//        ordenamiento.algoritmoShell(fourthList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoShell(fourthList, true);
+//
+//        System.out.println("\nQUICKSORT ----\n");
+//
+//        ordenamiento.algoritmoQuickSort(fourthList, false);
+//        System.out.println();
+//        ordenamiento.algoritmoQuickSort(fourthList, true);
 
 
     }
--- src/main/java/co/edu/ue/services/Ordenamiento.java
+++ src/main/java/co/edu/ue/services/Ordenamiento.java
@@ -5,82 +5,253 @@ import java.util.List;
 
 public class Ordenamiento {
 
-    public List<Integer> firstList = new ArrayList<>();
+    public int count = 0;
 
-    public List<Integer> secondList = new ArrayList<>();
+    public void imprimirCambios(List<Integer> list, Integer j, Integer target) {
+        int finalJ = j;
+        int finalTarget = target;
 
-    public List<Integer> thirdList = new ArrayList<>();
+        System.out.print(" [");
+        list.stream().forEach(c -> {
+            if (c.equals(list.get(finalJ))) {
+                if (c.equals(list.getFirst())) {
+                    System.out.print(list.get(finalTarget));
+                } else {
+                    System.out.print(", " + list.get(finalTarget));
+                }
+            } else if (c.equals(list.get(finalTarget))) {
+                System.out.print(", " + list.get(finalJ));
+            } else {
+                if (c.equals(list.getFirst())) {
+                    System.out.print(c);
+                } else {
+                    System.out.print(", " + c);
+                }
+            }
+        });
+        System.out.print("]");
 
-    public List<Integer> fourthList = new ArrayList<>();
-
-    public void addFirstElements() {
-        firstList.add(40);
-        firstList.add(21);
-        firstList.add(4);
-        firstList.add(9);
-        firstList.add(10);
-        firstList.add(35);
-    }
-    public void addSecondElements() {
-        secondList.add(47);
-        secondList.add(1);
-        secondList.add(5);
-        secondList.add(3);
-        secondList.add(7);
-        secondList.add(13);
-        secondList.add(25);
-        secondList.add(60);
-        secondList.add(0);
-    }
-    public void addThirdElements() {
-        thirdList.add(5);
-        thirdList.add(2);
-        thirdList.add(7);
-        thirdList.add(3);
-        thirdList.add(1);
-        thirdList.add(8);
-        thirdList.add(2);
-        thirdList.add(6);
-        thirdList.add(9);
-    }
-    public void addFourthElements() {
-        fourthList.add(3);
-        fourthList.add(5);
-        fourthList.add(33);
-        fourthList.add(1);
-        fourthList.add(8);
-        fourthList.add(1);
-        fourthList.add(24);
-        fourthList.add(23);
-        fourthList.add(8);
     }
 
-    public List<Integer> algoritmoSeleccion(List<Integer> listaDesordenada) {
+    public List<Integer> algoritmoSeleccion(List<Integer> listaDesordenada, boolean descendant) {
+        System.out.println("Algoritmo selección");
         System.out.println("Lista inicial: " + listaDesordenada);
         System.out.println("--------------------------------------");
 
         int aux;
-        int menor;
-
-
-        for(int i = 0; i < listaDesordenada.size(); i++){
-            menor = i;
-            for(int j = i + 1 ; j < listaDesordenada.size(); j++){
-                if(listaDesordenada.get(j) < listaDesordenada.get(menor)){
-                    menor = j;
+        int target;
+        int j;
+        for (int i = 0; i < listaDesordenada.size(); i++) {
+            target = i;
+            for (j = i + 1; j < listaDesordenada.size(); j++) {
+                //Condicion para saber si la lista se pide descendente o ascendente.
+                System.out.print("\nSe compara el " + listaDesordenada.get(j) + " con " + listaDesordenada.get(target) + ", ");
+                if (descendant) {
+                    if (listaDesordenada.get(j) > listaDesordenada.get(target)) {
+                        target = j;
+                        System.out.print("nuevo valor mayor: " + listaDesordenada.get(target));
+                    } else {
+                        System.out.print("no hay cambios.");
+                    }
+                } else {
+                    if (listaDesordenada.get(j) < listaDesordenada.get(target)) {
+                        target = j;
+                    }
                 }
             }
+            if(listaDesordenada.get(target) != listaDesordenada.get(i)){
+                System.out.println("\n~ Valor a cambiar: " +  listaDesordenada.get(target) + " con " + listaDesordenada.get(i));
+            }
+
             aux = listaDesordenada.get(i);
-            listaDesordenada.set(i, listaDesordenada.get(menor));
-            listaDesordenada.set(menor, aux);
+            listaDesordenada.set(i, listaDesordenada.get(target));
+            listaDesordenada.set(target, aux);
+            //Imprimimos las listas y su ordenamiento en cada pasada.
+            if (i == listaDesordenada.size() - 1) {
+                System.out.println("\n--------------------------------------");
+                System.out.println("Lista ordenada " + (descendant ? "descendente: " : "ascendente: ") + listaDesordenada);
+            }
+        }
+        //Un simple return por si queremos manejar la lista ordenada después.
+        return listaDesordenada;
+    }
 
-            System.out.println("Ordenación #" + (i+1) + ": "+ listaDesordenada);
+    public List<Integer> algoritmoBurbuja(List<Integer> listaDesordenada, boolean descendant) {
+        System.out.println("Algoritmo Burbuja");
+        System.out.println("Lista inicial: " + listaDesordenada);
+        System.out.println("--------------------------------------");
+        int aux;
+        for (int i = 0; i < listaDesordenada.size(); i++) {
+            for (int j = 0; j < (listaDesordenada.size() - i); j++) {
+                if ((j + 1) < listaDesordenada.size()) {
+                    if (descendant) {
+                        if (listaDesordenada.get(j + 1) > listaDesordenada.get(j)) {
+                            aux = listaDesordenada.get(j + 1);
+                            listaDesordenada.set(j + 1, listaDesordenada.get(j));
+                            listaDesordenada.set(j, aux);
+                            System.out.println("Pasada " + (i + 1) + ": " + listaDesordenada);
+                        }
+                    } else {
+                        if (listaDesordenada.get(j + 1) < listaDesordenada.get(j)) {
+                            aux = listaDesordenada.get(j + 1);
+                            listaDesordenada.set(j + 1, listaDesordenada.get(j));
+                            listaDesordenada.set(j, aux);
+                            System.out.println("Pasada " + (i + 1) + ": " + listaDesordenada);
+                        }
+                    }
+                }
+            }
+            if (i == listaDesordenada.size() - 1) {
+                System.out.println("--------------------------------------");
+                System.out.println("Lista ordenada " + (descendant ? "descendente: " : "ascendente: ") + listaDesordenada);
+            }
+        }
+        return listaDesordenada;
+    }
 
+    public List<Integer> algoritmoInsercionDirecta(List<Integer> listaDesordenada, boolean descendant) {
+        System.out.println("Algoritmo inserción directa");
+        System.out.println("Lista inicial: " + listaDesordenada);
+        System.out.println("--------------------------------------");
+        int aux;
+        int j;
+        for (int i = 0; i < listaDesordenada.size(); i++) {
+            aux = listaDesordenada.get(i);
+            for (j = i - 1; j >= 0; j--) {
+                if ((j + 1) < listaDesordenada.size()) {
+                    if (descendant) {
+                        if (aux < listaDesordenada.get(j)) {
+                            listaDesordenada.set((j + 1), aux);
+                            break;
+                        } else {
+                            listaDesordenada.set((j + 1), listaDesordenada.get(j));
+                        }
+                    } else {
+                        if (aux > listaDesordenada.get(j)) {
+                            listaDesordenada.set((j + 1), aux);
+                            break;
+                        } else {
+                            listaDesordenada.set((j + 1), listaDesordenada.get(j));
+                        }
+                    }
+                }
+            }
+            if (j == -1) {
+                listaDesordenada.set(0, aux);
+            }
+            System.out.println("Ordenando el " + aux + ": " + listaDesordenada);
         }
         System.out.println("--------------------------------------");
-        System.out.println("Lista final: " + listaDesordenada);
+        System.out.println("Lista ordenada " + (descendant ? "descendente: " : "ascendente: ") + listaDesordenada);
 
         return listaDesordenada;
     }
 
+    public List<Integer> algoritmoShell(List<Integer> listaDesordenada, boolean descendant) {
+        System.out.println("Algoritmo Shell");
+        System.out.println("Lista inicial: " + listaDesordenada);
+        System.out.println("------------------------------------------------");
+
+        int salto = listaDesordenada.size();
+        int aux;
+        for (int i = 0; i < listaDesordenada.size(); i++) {
+            if (salto > 1) salto /= 2;
+            for (int j = 0; j < listaDesordenada.size(); j++) {
+                aux = listaDesordenada.get(j);
+                if ((j + salto) < listaDesordenada.size()) {
+                    if (descendant) {
+                        if (aux < listaDesordenada.get(j + salto)) {
+                            listaDesordenada.set(j, listaDesordenada.get(j + salto));
+                            listaDesordenada.set((j + salto), aux);
+                            System.out.println("Ordenando con salto = " + salto + ": " + listaDesordenada + " ~ PASADA NUMERO: " + (i + 1));
+                        }
+                    } else {
+                        if (aux > listaDesordenada.get(j + salto)) {
+                            listaDesordenada.set(j, listaDesordenada.get(j + salto));
+                            listaDesordenada.set((j + salto), aux);
+                            System.out.println("Ordenando con salto = " + salto + ": " + listaDesordenada + " ~ PASADA NUMERO: " + (i + 1));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("------------------------------------------------");
+        System.out.println("Lista ordenada " + (descendant ? "descendente: " : "ascendente: ") + listaDesordenada);
+
+        return listaDesordenada;
+    }
+
+    public List<Integer> aoQuickSort(List<Integer> listaDesordenada, int inicio, int fin, boolean descendant) {
+        if (inicio >= fin) return listaDesordenada;
+        int pivote = listaDesordenada.get(inicio);
+        int left = inicio + 1;
+        int rigth = fin;
+        //Para validar si se solicitó descendente o ascendente
+        if (descendant) {
+            //Este if es para validar que se pidió la lista descendiente, si no, pasa al else
+            while (left <= rigth) {
+                while (left <= fin && listaDesordenada.get(left) > pivote) {
+                    left++;
+                }
+                while (rigth > inicio && listaDesordenada.get(rigth) <= pivote) {
+                    rigth--;
+                }
+                if (left < rigth) {
+                    int temp = listaDesordenada.get(left);
+                    listaDesordenada.set(left, listaDesordenada.get(rigth));
+                    listaDesordenada.set(rigth, temp);
+                } else {
+                    count++;
+                    System.out.println("Paso " + count + ": " + listaDesordenada);
+                }
+            }
+        } else {
+            //Este else es para imprimir la lista ordenada ascendentemente
+            while (left <= rigth) {
+                while (left <= fin && listaDesordenada.get(left) < pivote) {
+                    left++;
+                }
+                while (rigth > inicio && listaDesordenada.get(rigth) >= pivote) {
+                    rigth--;
+                }
+                if (left < rigth) {
+                    int temp = listaDesordenada.get(left);
+                    listaDesordenada.set(left, listaDesordenada.get(rigth));
+                    listaDesordenada.set(rigth, temp);
+                } else {
+                    count++;
+                    System.out.println("Paso " + count + ": " + listaDesordenada);
+                }
+            }
+        }
+        if (rigth > inicio) {
+            int temp = listaDesordenada.get(inicio);
+            listaDesordenada.set(inicio, listaDesordenada.get(rigth));
+            listaDesordenada.set(rigth, temp);
+            count++;
+            System.out.println("Paso " + count + ": " + listaDesordenada);
+        }
+        //Una vez no se ejecute más el while, se volverá a ejecutrar al propio algoritmo hasta que esté ordenada la listas
+        aoQuickSort(listaDesordenada, inicio, rigth - 1, descendant);
+        aoQuickSort(listaDesordenada, rigth + 1, fin, descendant);
+
+        return listaDesordenada;
+    }
+
+    public List<Integer> algoritmoQuickSort(List<Integer> listaDesordenada, boolean descendant) {
+        System.out.println("Algoritmo QuickSort");
+        System.out.println("Lista inicial: " + listaDesordenada);
+        System.out.println("------------------------------------------------");
+        //El tamaño de la lista menos 1. Para evitar errores de "out of bounds".
+        int length = (listaDesordenada.size() - 1);
+        //Reiniciamos contador de la clase, para que no se acumule con otras llamadas del algoritmo QuickSort
+        count = 0;
+        //Usar el algoritmo QuickSort para generar la lista ordenada. Solo es para mayor comodidad al leers
+        List<Integer> listaOrdenada = aoQuickSort(listaDesordenada, 0, length, descendant);
+        //Imprimir lista ordenada
+        System.out.println("------------------------------------------------");
+        System.out.println("Lista ordenada " + (descendant ? "descendente: " : "ascendente: ") + listaOrdenada);
+
+        return listaOrdenada;
+    }
 }
